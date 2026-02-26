6e56a5cb64d57e243d98c224e09612f7696ac104
hellsten <jessicahellsten@live.com>
2026-02-26 10:56:16 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
+import db from "./database.js";
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
