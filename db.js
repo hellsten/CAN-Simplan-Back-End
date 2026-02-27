adf47d263fac158e3f4e7437b34e73fba5c47331
Michael Corrado <michaelcorrado23@gmail.com>
2026-02-27 11:34:23 -0500

    Added directory structure setups and README placeholder

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
-import dotenv from "dotenv/config";
+import 'dotenv/config'
 import mysql from 'mysql2/promise'
-//dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import { v4 as uuidv4 } from "uuid";
-import connection from "./utils/database.js";
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE || process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+})
 
-const app = express();
-const PORT = process.env.PORT || 5050;
-
-app.use(cors());
-app.use(express.json());
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default connection
